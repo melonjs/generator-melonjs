c7c656ea526b3a1ba5ba04a8be61884fc5cc5abb
Ellison Leão <ellisonleao@gmail.com>
2014-08-28 20:13:48 -0300

    add grunt-contrib-connect for webserver

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -1,82 +1,98 @@
 /*global module:false*/
 'use strict';
 module.exports = function(grunt) {
-  var sourceFiles = [
-    'js/game.js',
-    'js/resources.js',
-    'js/screens/play.js',
-    'js/debugPanel.js',
-  ];
+    var sourceFiles = [
+        'js/game.js',
+        'js/resources.js',
+        'js/screens/play.js',
+        'js/debugPanel.js',
+    ];
 
-  // Project configuration.
-  grunt.initConfig({
-    concat: {
-      dist: {
-        src: sourceFiles,
-        dest: 'build/game.js'
-      }
-    },
-
-    uglify: {
-      options: {
-        report: 'min',
-        preserveComments: 'some'
-      },
-      dist: {
-        files: {
-          'build/game-min.js': [
-            sourceFiles
-          ]
-        }
-      }
-    },
-
-    jshint: {
-      options: {
-        jshintrc: ".jshintrc"
-      },
-
-      beforeConcat: {
-        files: {
-          src: sourceFiles
-        }
-      },
-
-      afterConcat: {
-        files: {
-          src: [ sourceFiles ]
-        }
-      }
-    },
-
-    clean: {
-      dist: [
-        'build/game.js',
-        'build/game-min.js'
-      ],
-    },
-
-    manifest: {
-      generate:{
-        options: {
-          basePath: '.',
-          cache: sourceFiles,
-          master: ['index.html']        
+    // Project configuration.
+    grunt.initConfig({
+        concat: {
+            dist: {
+                src: sourceFiles,
+                dest: 'build/game.js'
+            }
         },
-        src: sourceFiles + ['index.html', 'index.css', 'data/*', 'lib/*']     
-      }
-    }
 
-  });
+        uglify: {
+            options: {
+                report: 'min',
+                preserveComments: 'some'
+            },
+            dist: {
+                files: {
+                    'build/game-min.js': [
+                        sourceFiles
+                    ]
+                }
+            }
+        },
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-manifest');
+        jshint: {
+            options: {
+                jshintrc: ".jshintrc"
+            },
+
+            beforeConcat: {
+                files: {
+                    src: sourceFiles
+                }
+            },
+
+            afterConcat: {
+                files: {
+                    src: [ sourceFiles ]
+                }
+            }
+        },
+
+        clean: {
+            dist: [
+                'build/game.js',
+                'build/game-min.js'
+            ],
+        },
+
+        connect : {
+            server : {
+                options : {
+                    port : 8889
+                }
+            },
+
+            keepalive : {
+                options : {
+                    port : 8889,
+                    keepalive : true
+                }
+            }
+        },
+
+        manifest: {
+            generate:{
+                options: {
+                    basePath: '.',
+                    cache: sourceFiles,
+                    master: ['index.html']
+                },
+                src: sourceFiles + ['index.html', 'index.css', 'data/*', 'lib/*']
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-manifest');
+    grunt.loadNpmTasks("grunt-contrib-connect");
 
 
-  // Default task.
-  grunt.registerTask('default', ['concat', 'uglify', 'manifest']);
-  grunt.registerTask('lint', ['jshint:beforeConcat', 'concat', 'jshint:afterConcat']);
+    // Default task.
+    grunt.registerTask('default', ['concat', 'uglify', 'manifest']);
+    grunt.registerTask('lint', ['jshint:beforeConcat', 'concat', 'jshint:afterConcat']);
 };
