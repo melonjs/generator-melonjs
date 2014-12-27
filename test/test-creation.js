7c0f048c578d748ff49d48a4584da2c71a395c2c
Ellison Leão <ellisonleao@gmail.com>
2014-12-26 22:26:33 -0200

    fix tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -4,43 +4,46 @@ var path = require('path');
 var helpers = require('yeoman-generator').test;
 
 describe('melonjs generator', function () {
-  beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-      if (err) {
-        return done(err);
-      }
+    beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+            if (err) {
+                return done(err);
+            }
 
-      this.app = helpers.createGenerator('melonjs:app', [
-        '../../app'
-      ]);
-      done();
-    }.bind(this));
-  });
-
-  it('creates expected files', function (done) {
-    var expected = [
-      // add files you expect to exist here.
-      '.jshintrc',
-      '.gitignore',
-      'package.json',
-      'bower.json',
-      'Gruntfile.js',
-      'index.html',
-      'css/index.css',
-      'data/img/bg.png',
-      'data/img/melonjs.png',
-      'js/game.js',
-      'js/screens/play.js',
-      'js/debugPanel.js',
-    ];
-
-    helpers.mockPrompt(this.app, {
-      'someOption': true
+            this.app = helpers.createGenerator('melonjs:app', [
+                '../../app'
+            ]);
+            done();
+        }.bind(this));
     });
-    this.app.options['skip-install'] = true;
-    this.app.run({}, function () {
-      helpers.assertFile(expected);
-      done();
+
+    it('creates expected files', function (done) {
+        var expected = [
+            // add files you expect to exist here.
+            '.jshintrc',
+            '.gitignore',
+            'package.json',
+            'bower.json',
+            'Gruntfile.js',
+            'index.html',
+            'css/index.css',
+            'data/img/bg.png',
+            'data/img/melonjs.png',
+            'js/game.js',
+            'js/screens/play.js',
+            'js/debugPanel.js',
+        ];
+
+        helpers.mockPrompt(this.app, {
+            'gameName': 'test'
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+            for (var i = 0; i < expected.length; i++) {
+                var filename = path.join(__dirname, 'temp', 'test', expected[i]);
+                helpers.assertFile(filename);
+            }
+            done();
+        });
     });
-  });
 });
